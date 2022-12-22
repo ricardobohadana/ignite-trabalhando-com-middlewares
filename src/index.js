de1a54965ca22002ffa7249576e49836d9247010
ricardobohadana <cacolorde@gmail.com>
2022-12-22 15:49:30 -0300

    solucao

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const username = request.headers.username;
+  if(!username)
+    return response.status(400).json({error: 'Username nao enviado no header'});
+
+  const user = users.find(u => u.username === username);
+  if(!user)
+    return response.status(404).json({error: `Username ${user} nao encontrado`});
+  
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  
+  if(user.pro)
+    return next();
+
+  if(user.todos.length < 10)
+    return next();
+
+  return response.status(403).json({error: 'Este usuario ja atingiu o limite'});
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+
+  const username = request.headers.username;
+
+  if(!username)
+    return response.status(400).json({error: 'Username nao enviado no header'});
+
+  const user = users.find(u => u.username === username);
+  if(!user)
+    return response.status(404).json({error: `Username ${user} nao encontrado`});
+  
+  const { id } = request.params;
+  if(!id) return response(400).json({error: 'id nao enviado'})
+
+  const isUuid = validate(id);
+  console.log(isUuid, id);
+  if(!isUuid) return response.status(400).json({error: 'id invalido'});
+  
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo) 
+    return response.status(404).json({error: 'todo nao pertence a este user'});
+  
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(u => u.id === id);
+  if(!user)
+    return response.status(404).json({
+      error: "Usuario com id "+ request.params.id +" não encontrado."
+    });
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -106,7 +154,7 @@ app.patch('/todos/:id/done', checksTodoExists, (request, response) => {
   return response.json(todo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, response) => {
+app.delete('/todos/:id', checksTodoExists, (request, response) => {
   const { user, todo } = request;
 
   const todoIndex = user.todos.indexOf(todo);
